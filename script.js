76fdb15b24c4fb6e9cbfe66fd9415b8916d8fbf2
Nathan Reents <130861854+NJRFirstOfHisName@users.noreply.github.com>
2023-06-07 18:33:08 -0600

    Adjust logic to make matches best of 5
    TODO: Make game reset after a match completes.

--- script.js
+++ script.js
@@ -6,96 +6,110 @@ function getComputerChoice() {
         return "paper"
     } else if (rps < 3) {
         return "scissors"
-    } else {
-        return "Invalid input, error in random function"
     }
 }
 
 function playRound(playerSelection) {
-
-    // let playerSelection = prompt("Enter your choice: rock, paper, or scissors?").toLowerCase();
     const computerSelection = getComputerChoice();
-    if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
-        if (playerSelection === "rock") {
-            if (computerSelection === "rock") {
-                console.log("Computer has selected rock. Tie game!")
-                return 0
-            } else if (computerSelection === "paper") {
-                console.log("Computer has selected paper. You lose!")
-                return 1
-            } else {
-                console.log("Computer has selected scissors. You win!")
-                return 2
-            }
-        } else if (playerSelection === "paper") {
-            if (computerSelection === "rock") {
-                console.log("Computer has selected rock. You win!")
-                return 2
-            } else if (computerSelection === "paper") {
-                console.log("Computer has selected paper. Tie game!")
-                return 0
-            } else {
-                console.log("Computer has selected scissors. You lose!")
-                return 1
-            } 
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
+            div.textContent = "Computer has selected rock. Tie game!"
+            return 0
+        } else if (computerSelection === "paper") {
+            div.textContent = "Computer has selected paper. You lose!"
+            return 1
         } else {
-            if (computerSelection === "rock") {
-                console.log("Computer has selected rock. You lose!")
-                return 1
-            } else if (computerSelection === "paper") {
-                console.log("Computer has selected paper. You win!")
-                return 2
-            } else {
-                console.log("Computer has selected scissors. Tie game!")
-                return 0
-            }
+            div.textContent = "Computer has selected scissors. You win!"
+            return 2
         }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            div.textContent = "Computer has selected rock. You win!"
+            return 2
+        } else if (computerSelection === "paper") {
+            div.textContent = "Computer has selected paper. Tie game!"
+            return 0
+        } else {
+            div.textContent = "Computer has selected scissors. You lose!"
+            return 1
+        } 
     } else {
-        playerSelection = prompt("Invalid entry, please try again.")
+        if (computerSelection === "rock") {
+            div.textContent = "Computer has selected rock. You lose!"
+            return 1
+        } else if (computerSelection === "paper") {
+            div.textContent = "Computer has selected paper. You win!"
+            return 2
+        } else {
+            div.textContent = "Computer has selected scissors. Tie game!"
+            return 0
+        }
     }
 }
 
 function game() {
     wins = 0
-    losses = 0;
-    for (i = 0; i < 5; i=5) {
-        result = playRound()
-        if (result === 1) {
-            losses++
-        } else if (result === 2){
-            wins++
-        }
-        if (wins < losses) {
-            console.log("After " + (i+1) + " round(s) you are behind by " + (losses-wins));
-        } else if (wins > losses) {
-            console.log("After " + (i+1) + " round(s) you are ahead by " + (wins-losses));
-        } else {
-            console.log("After " + (i+1) + " round(s) you are tied!")
-        }
-        if ((wins-losses) > 2) {
-            console.log("You win the match!")
-            i = 5
-        } else if ((losses-wins) > 2) {
-            console.log("You lose the match.")
-            i = 5
-        } else if (i === 4) {
-            if (wins > losses) {
-                console.log("You've won the match!")
-            } else if (wins < losses) {
-                console.log("You've lost the match!")
-            } else {
-                console.log("The match has ended in a tie.")
+    losses = 0
+    i = 0
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            result = playRound(button.id)
+            i++
+            if (result === 1) {
+                losses++
+            } else if (result === 2){
+                wins++
             }
-        }
-    }
+            div.textContent += "\r\nWins: " + wins + "\r\nLosses: " + losses
+                if (wins === 5 || losses === 5) {
+                    if (wins > losses) {
+                        div.textContent += "\n You win the match!"
+                        game()
+                    } else {
+                        div.textContent += "\n You lose the match."
+                        game()
+                    }
+                    wins = 0
+                    losses = 0
+                    i = 0
+            }
+        })
+    })
+
+    // for (i = 0; i < 5; i = 5) {
+    //     if (result === 1) {
+    //         losses++
+    //     } else if (result === 2){
+    //         wins++
+    //     }
+    //     if (wins < losses) {
+    //         div.textContent += "After " + (i+1) + " round(s) you are behind by " + (losses-wins);
+    //     } else if (wins > losses) {
+    //         div.textContent += "After " + (i+1) + " round(s) you are ahead by " + (wins-losses);
+    //     } else {
+    //         div.textContent += "After " + (i+1) + " round(s) you are tied!"
+    //     }
+    //     if ((wins-losses) > 2) {
+    //         div.textContent += "You win the match!"
+    //         i = 5
+    //     } else if ((losses-wins) > 2) {
+    //         div.textContent += "You lose the match."
+    //         i = 5
+    //     } else if (i === 4) {
+    //         if (wins > losses) {
+    //             div.textContent += "You've won the match!"
+    //         } else if (wins < losses) {
+    //             div.textContent += "You've lost the match!"
+    //         } else {
+    //             div.textContent += "The match has ended in a tie."
+    //         }
+    //     }
+    // }
 }
 
+const div = document.createElement('div');
+div.setAttribute('style', 'white-space: pre;')
+container.appendChild(div);
 const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        console.log(button.id);
-        playRound(button.id)
-    })
-})
 
-// game();
+game();
